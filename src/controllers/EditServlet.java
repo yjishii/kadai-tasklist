451728273ad52aebbbf6818d65bb3d647362a4d2
yoji.ishii <taro.kirameki@techacademy.jp>
2020-10-11 02:02:23 -0400

    Add CreateServlet.java, DestroyServlet.java, EditServlet.java, IndexServlet.java, NewServlet.java, ShowServlet.java, UpdateServlet.java

--- src/controllers/EditServlet.java
+++ src/controllers/EditServlet.java
@@ -1,7 +1,6 @@
 package controllers;
 
 import java.io.IOException;
-import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.servlet.RequestDispatcher;
@@ -15,18 +14,17 @@ import models.Task;
 import utils.DBUtil;
 
 /**
- * Servlet implementation class IndexServlet
+ * Servlet implementation class EditServlet
  */
-@WebServlet("/index")
-public class IndexServlet extends HttpServlet {
+@WebServlet("/edit")
+public class EditServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public IndexServlet() {
+    public EditServlet() {
         super();
-
     }
 
     /**
@@ -35,17 +33,22 @@ public class IndexServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
 
-        List<Task> tasks = em.createNamedQuery("getAllTasks", Task.class)
-                            .getResultList();
+        Task m = em.find(Task.class, Integer.parseInt(request.getParameter("id")));
+
         em.close();
 
-        request.setAttribute("tasks", tasks);
+        request.setAttribute("task", m);
+        request.setAttribute("_token",  request.getSession().getId());
 
-        request.getSession().removeAttribute("task_id");
+        if(m != null){
+            request.getSession().setAttribute("task_id", m.getId());
+        }
 
-        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/index.jsp");
-        rd.forward(request,  response);
 
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/edit.jsp");
+        rd.forward(request, response);
     }
+
 
 }
